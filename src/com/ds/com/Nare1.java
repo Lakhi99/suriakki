c4c6411a01dc583fcd01c5bee69d0318bd7b888c
Lakhi99 <lingaakhila82@gmail.com>
2026-01-23 18:48:31 +0530

    Nari1

---
+++ src/com/ds/com/Nare1.java
@@ -0,0 +1,11 @@
+package com.ds.com;
+
+public class Nare1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("akhi");
+
+	}
+
+}
---
+++ src/com/ds/com/Test5.java
@@ -0,0 +1,11 @@
+package com.ds.com;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("linga");
+
+	}
+
+}
